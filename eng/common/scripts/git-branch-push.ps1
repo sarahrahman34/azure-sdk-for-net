139aaf549f4ffe5ca3f9fbfa0f2737c8e7285c82
sima-zhu <sizhu@microsoft.com>
2022-08-18 13:23:08 -0700

    Sync eng/common directory with azure-sdk-tools repository for Tools PR 3981

--- eng/common/scripts/git-branch-push.ps1
+++ eng/common/scripts/git-branch-push.ps1
@@ -109,7 +109,9 @@ do
 {
     $needsRetry = $false
     Write-Host "git push $RemoteName $PRBranchName $PushArgs"
-    git push $RemoteName $PRBranchName $PushArgs
+    if ($PSCmdlet.ShouldProcess("Push to [ $RemoteName ] [ $PRBranchName ]", "Push local changes to remote branch.")) {
+        git push $RemoteName $PRBranchName $PushArgs
+    }
     $tryNumber++
     if ($LASTEXITCODE -ne 0)
     {
